97130834504c54437a93da7eee322266d5fb60b9
JonasBa <jonas@badalic.com>
2022-11-11 08:47:25 -0500

    fib

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ const transaction = Sentry.startTransaction({
     name: "My First Test Transaction",
 });
 
+const fib = (n) => {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+};
 
 function exception(){
     return new Promise((resolve) => {
+        fib(34)
         setTimeout(() => {
             try {
                 foo();
